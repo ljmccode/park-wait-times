2130e82c8e88fa1c0e4b49950b8f05db26b09f4d
Laura <lmcsay@gmail.com>
2022-08-22 16:43:46 -0500

    Schedule wait times to be pulled every hour

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cron from 'node-cron';
 import { connectDB } from './db.js';
-import { getHollywoodWaitTimes } from './controllers/hollywoodWaitTimes.js';
-import { getEpcotWaitTimes } from './controllers/epcotWaitTimes.js';
+import { getWaitTimeData } from './controllers/getWaitData.js';
 
 dotenv.config();
 const app = express();
@@ -13,8 +13,8 @@ app.use(express.json());
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    await getHollywoodWaitTimes();
-    await getEpcotWaitTimes();
+    await cron.schedule('0 * * * *', () => getWaitTimeData());
+    // getWaitTimeData();
     app.listen(port, () => {
       console.log(`Now listening on port ${port}`);
     });
