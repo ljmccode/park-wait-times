11276521abad4e759f15886d0753727e47191b51
Laura <lmcsay@gmail.com>
2022-08-22 17:22:49 -0500

    Make basic model structures for both parks

---
+++ server/models/epcot.model.js
@@ -0,0 +1,11 @@
+import EpcotWaitTime from './epcot.mongo.js';
+
+// get all Epcot ride wait times for the day
+export const getAllEpcotRides = async () => {
+  console.log('All Hollywood Studios Rides');
+};
+
+// get all specific Epcot ride wait times for the day
+export const getEpcotRide = async (rideId) => {
+  console.log(`Epcot ride with id of ${rideId}`);
+};
---
+++ server/models/hollywoodStudios.model.js
@@ -0,0 +1,11 @@
+import HollywoodWaitTime from './hollywoodStudios.model';
+
+// get all HS ride wait times for the day
+export const getAllHSRides = async () => {
+  console.log('All Hollywood Studios Rides');
+};
+
+// get all specific HS ride wait times for the day
+export const getHSRide = async (rideId) => {
+  console.log(`Hollywood ride with id of ${rideId}`);
+};
