a31d899ffba43a1665ce92efb0eea32aab293ae6
Laura <lmcsay@gmail.com>
2022-08-23 11:59:24 -0500

    Clean up get all ride models

--- server/models/epcot.model.js
+++ server/models/epcot.model.js
@@ -2,13 +2,7 @@ import mongoose from 'mongoose';
 import EpcotWaitTime from './epcot.mongo.js';
 
 export const getAllEpcotRides = async (time, date) => {
-  const slicedTime = time.slice(1, 6);
-  const slicedDate = date.slice(1, 11);
-  const queryObject = {
-    time: slicedTime,
-    date: slicedDate,
-  };
-  return await EpcotWaitTime.find(queryObject);
+  return await EpcotWaitTime.find({ time, date });
 };
 
 export const getEpcotRide = async (rideName) => {
--- server/models/hollywoodStudios.model.js
+++ server/models/hollywoodStudios.model.js
@@ -2,13 +2,7 @@ import mongoose from 'mongoose';
 import HollywoodWaitTime from './hollywoodStudios.mongo.js';
 
 export const getAllHSRides = async (time, date) => {
-  const slicedTime = time.slice(1, 6);
-  const slicedDate = date.slice(1, 11);
-  const queryObject = {
-    time: slicedTime,
-    date: slicedDate,
-  };
-  return await HollywoodWaitTime.find(queryObject);
+  return await HollywoodWaitTime.find({ time, date });
 };
 
 export const getHSRide = async (rideName) => {
