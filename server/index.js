f8f02598d1eea0d64fd17f66cfd667ad4e4fabd1
Laura <lmcsay@gmail.com>
2022-08-22 20:54:06 -0500

    Add park routes to app

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cron from 'node-cron';
+
 import { connectDB } from './db.js';
 import { getWaitTimeData } from './getAPIdata/getWaitData.js';
+import epcotRouter from './routes/epcot.js';
+import hollywoodRouter from './routes/hollywood.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use('/api/v1/epcot', epcotRouter);
+app.use('/api/v1/hollywood-studios', hollywoodRouter);
 
 const start = async () => {
   try {
