a80a99529a904a484ffb15f0808f6910622786f7
Laura <lmcsay@gmail.com>
2022-08-24 13:31:40 -0500

    Add Park component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import Navbar from './components/Navbar';
+import Parks from './components/Parks';
 import './App.css';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Parks />
+    </>
+  );
 }
 
 export default App;
