4389173d6bd2ff47e0de06ac9840d50237a95d8a
Laura <lmcsay@gmail.com>
2022-08-26 13:19:40 -0500

    Create a get Hollywood Studios thunk to get rides for certain date & time

--- client/src/features/waitTimes/waitTimesSlice.js
+++ client/src/features/waitTimes/waitTimesSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const currentDate = new Date();
 const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
@@ -9,7 +10,7 @@ const formattedDate = `${month}/${date}/${year}`;
 
 const initialFilterState = {
   date: formattedDate,
-  militaryTime: '12:49',
+  militaryTime: '12:00',
   time: '12:00 PM',
 };
 
@@ -19,9 +20,35 @@ const initialState = {
   ...initialFilterState,
 };
 
+export const getHSTimes = createAsyncThunk(
+  'waitTimes/getHSTimes',
+  async (_, thunkAPI) => {
+    const { militaryTime, date } = thunkAPI.getState().waitTimes;
+    let url = `/api/v1/hollywood-studios?time=${militaryTime}&date=${date}`;
+    try {
+      const { data } = await axios(url);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 const waitTimesSlice = createSlice({
   name: 'waitTimes',
   initialState,
+  extraReducers: {
+    [getHSTimes.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getHSTimes.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.waitTimes = payload;
+    },
+    [getHSTimes.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
 });
 
 export default waitTimesSlice.reducer;
