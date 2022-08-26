439a9a9a47283e7ef0507fd79435e9f5942d5107
Laura <lmcsay@gmail.com>
2022-08-26 17:06:08 -0500

    Sort data by name alphabetically

--- server/models/hollywoodStudios.model.js
+++ server/models/hollywoodStudios.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import HollywoodWaitTime from './hollywoodStudios.mongo.js';
 
 export const getAllHSRides = async (time, date) => {
-  return await HollywoodWaitTime.find({ time, date });
+  return await HollywoodWaitTime.find({ time, date }).sort('name');
 };
 
 export const getHSRide = async (rideName) => {
