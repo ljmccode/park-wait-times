a05167a14841eb00b12c8b81f08dcd84469f8a4a
Laura <lmcsay@gmail.com>
2022-08-27 12:17:17 -0500

    Update models to find a ride by a specific date

--- server/models/hollywoodStudios.model.js
+++ server/models/hollywoodStudios.model.js
@@ -5,6 +5,6 @@ export const getAllHSRides = async (time, date) => {
   return await HollywoodWaitTime.find({ time, date }).sort('name');
 };
 
-export const getHSRide = async (rideName) => {
-  return await HollywoodWaitTime.find({ name: rideName });
+export const getHSRide = async (rideName, date) => {
+  return await HollywoodWaitTime.find({ name: rideName, date: date });
 };
