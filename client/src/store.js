5da7228454b6fb8b3b075d199bf13a3fc2f2027c
Laura <lmcsay@gmail.com>
2022-08-30 09:54:35 -0500

    Install react-icons

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import waitTimesSlice from './features/waitTimes/waitTimesSlice';
+import sortSlice from './features/waitTimes/sortSlice';
+
+const rootReducer = combineReducers({
+  waitTimes: waitTimesSlice,
+  sort: sortSlice,
+});
 
 export const store = configureStore({
-  reducer: {
-    waitTimes: waitTimesSlice,
-  },
+  reducer: rootReducer,
 });
