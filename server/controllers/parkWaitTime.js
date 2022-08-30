82279ada7ed7b442699e2fe7fa37938df4c43994
Laura <lmcsay@gmail.com>
2022-08-30 09:59:50 -0500

    Sort rides dynamically by the sort query parameter

--- server/controllers/parkWaitTime.js
+++ server/controllers/parkWaitTime.js
@@ -2,16 +2,15 @@ import { getAllEpcotRides, getEpcotRide } from '../models/epcot.model.js';
 import { getAllHSRides, getHSRide } from '../models/hollywoodStudios.model.js';
 
 export const httpGetAllRides = async (req, res) => {
-  const { time, date } = req.query;
+  const { time, date, sort } = req.query;
   const { park } = req.params;
-
   if (park === 'epcot') {
-    const rides = await getAllEpcotRides(time, date);
+    const rides = await getAllEpcotRides(time, date, sort);
     return res.status(200).json(rides);
   }
 
   if (park === 'hollywood-studios') {
-    const rides = await getAllHSRides(time, date);
+    const rides = await getAllHSRides(time, date, sort);
     return res.status(200).json(rides);
   }
 };
