fef92a43792581c5745e9f5666f937aeaaca3d87
Laura <lmcsay@gmail.com>
2022-08-30 11:19:15 -0500

    Add time ascending to wait time caret

--- client/src/components/Caret.js
+++ client/src/components/Caret.js
@@ -2,10 +2,9 @@ import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai';
 import { useSelector } from 'react-redux';
 
 const Caret = () => {
-  const { nameAscending, waitAscending, statusAscending } = useSelector(
-    (store) => store.sort
-  );
-  return nameAscending || waitAscending || statusAscending ? (
+  const { nameAscending, waitAscending, statusAscending, timeAscending } =
+    useSelector((store) => store.sort);
+  return nameAscending || waitAscending || statusAscending || timeAscending ? (
     <AiFillCaretDown className='caret' />
   ) : (
     <AiFillCaretUp className='caret' />
