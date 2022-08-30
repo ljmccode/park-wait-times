d15a3ae1f617f9799370d1359695b61e01f98d22
Laura <lmcsay@gmail.com>
2022-08-30 09:57:19 -0500

    Sort rides dynamically by the sort query parameter

--- server/models/epcot.model.js
+++ server/models/epcot.model.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 import EpcotWaitTime from './epcot.mongo.js';
 
-export const getAllEpcotRides = async (time, date) => {
-  return await EpcotWaitTime.find({ time, date });
+export const getAllEpcotRides = async (time, date, sort) => {
+  return await EpcotWaitTime.find({ time, date }.sort(`${sort}`));
 };
 
 export const getEpcotRide = async (rideName, date) => {
--- server/models/hollywoodStudios.model.js
+++ server/models/hollywoodStudios.model.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 import HollywoodWaitTime from './hollywoodStudios.mongo.js';
 
-export const getAllHSRides = async (time, date) => {
-  return await HollywoodWaitTime.find({ time, date }).sort('name');
+export const getAllHSRides = async (time, date, sort) => {
+  return await HollywoodWaitTime.find({ time, date }).sort(`${sort}`);
 };
 
 export const getHSRide = async (rideName, date) => {
