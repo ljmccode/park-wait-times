c7d9b90a69d58b26788cdbc0652a9350423e7355
Laura <lmcsay@gmail.com>
2022-09-01 14:43:11 -0500

    Update mongo connection to call error

--- server/db.js
+++ server/db.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 
 export const connectDB = (url) => {
-  return mongoose.connect(url);
+  console.log(url);
+  return mongoose.connect(url, _, (err) => {
+    if (err) console.log(err);
+    else console.log('mongodb is connected');
+  });
 };
