2bb2dd1001d3fc0c7ca0379b97e54605dc3399af
Laura <lmcsay@gmail.com>
2022-08-31 22:46:02 -0500

    Update server for production

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ import { connectDB } from './db.js';
 // import { getWaitTimeData } from './getAPIdata/getWaitData.js';
 import parkRouter from './routes/parkWaitTime.js';
 
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,6 +18,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', parkRouter);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const start = async () => {
   try {
