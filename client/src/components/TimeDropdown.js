c6798cb7025482589ca835ac0f43255d76b9b84a
Laura <lmcsay@gmail.com>
2022-09-02 19:37:08 -0500

    Only show availabile times for that day in the dropdown

--- client/src/components/TimeDropdown.js
+++ client/src/components/TimeDropdown.js
@@ -3,25 +3,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import Select from './Select';
 import {
   handleChange,
-  updateTime,
   updateMilitaryTime,
 } from '../features/waitTimes/waitTimesSlice';
-import { openHours, getFloridaTime, convertMilitary } from '../utils/hours';
+import { convertMilitary, convertRegularTime } from '../utils/hours';
 
 const TimeDropdown = () => {
   const dispatch = useDispatch();
-  const { time } = useSelector((store) => store.waitTimes);
+  const { time, availableTimes } = useSelector((store) => store.waitTimes);
+  const dropdownTimes = availableTimes
+    ? availableTimes.map((time) => convertRegularTime(time))
+    : ['11:00 AM'];
 
   const handleTimeInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     dispatch(handleChange({ name, value }));
   };
-
-  useEffect(() => {
-    const currentFloridaTime = getFloridaTime();
-    dispatch(updateTime(currentFloridaTime));
-  }, []);
 
   useEffect(() => {
     const militaryTime = convertMilitary(time);
@@ -32,9 +29,13 @@ const TimeDropdown = () => {
     <>
       <Select
         name={'time'}
-        value={time}
+        value={
+          dropdownTimes.includes(time)
+            ? time
+            : dropdownTimes[dropdownTimes.length - 1]
+        }
         handleChange={handleTimeInput}
-        options={openHours}
+        options={dropdownTimes}
       />
     </>
   );
