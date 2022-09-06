708472539331f45f7d6a6f58ea2dffc1f283b78b
Laura <lmcsay@gmail.com>
2022-09-06 11:52:44 -0500

    Use try catch with wait time API

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export const connectDB = (url) => {
+export const connectDB = async (url) => {
   console.log('connecting to database!');
-  return mongoose.connect(url);
+  return await mongoose.connect(url);
 };
--- server/getAPIdata/getWaitData.js
+++ server/getAPIdata/getWaitData.js
@@ -5,34 +5,38 @@ import { getEpcotWaitTimes } from './epcotWaitTimes.js';
 import { getMKWaitTimes } from './magicKingdomWaitTimes.js';
 import { getAKWaitTimes } from './animalKingdomWaitTimes.js';
 import { connectDB } from '../db.js';
-import mongoose from 'mongoose';
 
 dotenv.config();
 
 const getWaitTimeData = async () => {
-  await connectDB(process.env.MONGO_URI);
-  let currentTime = new Date().getTime();
-  let formattedTime = Number(
-    moment(currentTime).tz('America/New_York').format('HH')
-  );
-  if (formattedTime < 7) {
+  try {
+    await connectDB(process.env.MONGO_URI);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    let currentTime = new Date().getTime();
+    let formattedTime = Number(
+      moment(currentTime).tz('America/New_York').format('HH')
+    );
+    if (formattedTime < 7) {
+      console.log(
+        `parks are closed at ${moment(new Date())
+          .tz('America/Chicago')
+          .format('hh:mm')} Central Time`
+      );
+      return;
+    }
+    console.log('about to call APIs');
+    await getHollywoodWaitTimes();
+    await getEpcotWaitTimes();
+    await getMKWaitTimes();
+    await getAKWaitTimes();
     console.log(
-      `parks are closed at ${moment(new Date())
+      `data pulled at ${moment(new Date())
         .tz('America/Chicago')
         .format('hh:mm')} Central Time`
     );
-    return;
   }
-  console.log('about to call APIs');
-  await getHollywoodWaitTimes();
-  await getEpcotWaitTimes();
-  await getMKWaitTimes();
-  await getAKWaitTimes();
-  console.log(
-    `data pulled at ${moment(new Date())
-      .tz('America/Chicago')
-      .format('hh:mm')} Central Time`
-  );
 };
 
 getWaitTimeData();
