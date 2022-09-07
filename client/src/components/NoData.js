f73b265840d1836afbe836bfb7226963c0340c99
Laura <lmcsay@gmail.com>
2022-09-07 12:06:29 -0500

    Update No Data component to say parks are closed and to choose another day

--- client/src/components/NoData.js
+++ client/src/components/NoData.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import sadMickey from '../assets/images/sadMickey.png';
+import {
+  changeDate,
+  updateParkStatus,
+  updateTime,
+} from '../features/waitTimes/waitTimesSlice';
+import moment from 'moment';
+import 'moment-timezone';
 
-const NoData = ({ time }) => {
+const NoData = () => {
+  const dispatch = useDispatch();
+
+  const yesterday = moment(new Date())
+    .tz('America/New_York')
+    .subtract(1, 'day')
+    .format('MM/DD/YYYY');
+
+  const yesterdayTimes = () => {
+    dispatch(updateTime({ time: '4:00 PM' }));
+    dispatch(changeDate(yesterday));
+    dispatch(updateParkStatus(true));
+  };
+
   return (
     <Wrapper>
-      <p>No wait time data available for {time}</p>
+      <p>
+        Shucks, the parks are closed!{' '}
+        <button onClick={yesterdayTimes}>
+          Check wait times for another day
+        </button>
+      </p>
       <img src={sadMickey} alt='Sad Mickey' />
     </Wrapper>
   );
@@ -17,7 +43,19 @@ const Wrapper = styled.div`
   p {
     font-weight: 1rem;
     text-align: center;
-    margin-bottom: 3rem;
+    margin-bottom: 2rem;
+    margin-top: 0;
+
+    button {
+      border: none;
+      background: none;
+      color: var(--green-600);
+      cursor: pointer;
+      font-size: 1rem;
+    }
+    button:hover {
+      text-decoration: underline;
+    }
   }
 
   img {
