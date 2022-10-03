8f1273e66def2ff72f24e3264d939764465092cc
Laura <lmcsay@gmail.com>
2022-10-03 17:15:35 -0500

    Move sorting info out of async park thunks since sorting logic was moved to client side

--- client/src/features/waitTimes/waitTimesThunk.js
+++ client/src/features/waitTimes/waitTimesThunk.js
@@ -1,24 +1,8 @@
 import axios from 'axios';
 
 export const getParkTimesThunk = async (_, thunkAPI) => {
-  let { currentSort, waitAscending, nameAscending, statusAscending } =
-    thunkAPI.getState().sort;
-
-  switch (currentSort) {
-    case 'name':
-      currentSort = nameAscending ? currentSort : `-${currentSort}`;
-      break;
-    case 'waitTime':
-      currentSort = waitAscending ? `-${currentSort}` : currentSort;
-      break;
-    case 'status':
-      currentSort = statusAscending ? currentSort : `-${currentSort}`;
-      break;
-    default:
-      currentSort = 'name';
-  }
-  const { militaryTime, date, currentPark } = thunkAPI.getState().waitTimes;
-  let url = `/api/v1/${currentPark}?time=${militaryTime}&date=${date}&sort=${currentSort}`;
+  const { date, currentPark } = thunkAPI.getState().waitTimes;
+  let url = `/api/v1/${currentPark}?date=${date}`;
   try {
     const { data } = await axios(url);
     return data;
@@ -27,26 +11,9 @@ export const getParkTimesThunk = async (_, thunkAPI) => {
   }
 };
 
-export const viewRideInfoThunk = async (name, thunkAPI) => {
-  let { currentSort, waitAscending, timeAscending, statusAscending } =
-    thunkAPI.getState().sort;
-
-  switch (currentSort) {
-    case 'time':
-      currentSort = timeAscending ? currentSort : `-${currentSort}`;
-      break;
-    case 'waitTime':
-      currentSort = waitAscending ? `-${currentSort}` : currentSort;
-      break;
-    case 'status':
-      currentSort = statusAscending ? currentSort : `-${currentSort}`;
-      break;
-    default:
-      currentSort = 'time';
-  }
-  const { date, currentPark } = thunkAPI.getState().waitTimes;
-  const urlName = encodeURIComponent(name);
-  let url = `/api/v1/${currentPark}/${urlName}?date=${date}&sort=${currentSort}`;
+export const getCurrentWaitTimesThunk = async (_, thunkAPI) => {
+  const { currentPark } = thunkAPI.getState().waitTimes;
+  let url = `/api/v1/currentWait/${currentPark}`;
   try {
     const { data } = await axios(url);
     return data;
